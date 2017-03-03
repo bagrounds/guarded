8e81e70cea6e1cd904ea197baf7ff84dbe87de63
bagrounds <bagrounds@gmail.com>
2017-03-03 01:33:48 -0800

    [MINOR][CHANGE] implementation to take a single options parameter with input, f, and output fields

--- src/index.js
+++ src/index.js
@@ -2,51 +2,24 @@
   'use strict'
 
   /* imports */
-  var optionsChecker = require('./spec-options')
-  var outputChecker = require('./spec-output')
+  var funPredicate = require('fun-predicate')
+  var compose = require('fun-compose')
 
   /* exports */
-  module.exports = guardedGuarded
-
-  function guardedGuarded (options) {
-    return outputChecker(guarded(optionsChecker(options)))
-  }
+  module.exports = guarded
 
   /**
    *
    * @param {Object} options all input parameters
-   * @param {Function} options.input checks input
-   * @param {Function} options.output checks output
-   * @param {Boolean} options.sync guard a synchronous function
-   * @return {Function} that takes a function and returns a guarded function
+   * @param {Function} options.input - contract
+   * @param {Function} options.f - function to guard
+   * @param {Function} options.output - contract
+   * @return {Function} f guarded with input and output contracts
    */
   function guarded (options) {
-    var inputChecker = options.input
-    var outputChecker = options.output
+    funPredicate.type('{input: Function, f: Function, output: Function}')
 
-    if (options.sync) {
-      return syncGuardConstructor(inputChecker, outputChecker)
-    }
-
-    return asyncGuardConstructor(inputChecker, outputChecker)
-  }
-
-  function syncGuardConstructor (inputChecker, outputChecker) {
-    return function syncGuard (vulnerable) {
-      return function (input) {
-        return outputChecker(vulnerable(inputChecker(input)))
-      }
-    }
-  }
-
-  function asyncGuardConstructor (inputChecker, outputChecker) {
-    return function asyncGuard (vulnerable) {
-      return function (options, callback) {
-        vulnerable(inputChecker(options), function (error, result) {
-          callback(error, outputChecker(result))
-        })
-      }
-    }
+    return [options.output, options.f, options.input].reduce(compose)
   }
 })()
 
